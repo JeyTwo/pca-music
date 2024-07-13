04f71c46bac9c45201c410421bf2c7dd94657e37
Jhon Maldonado <jhonmaldonado01@gmail.com>
2024-07-12 20:56:03 -0500

    Agregando funciones de Router y Storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,37 +7,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  slides = [
-    {
-      title: "Titulo de slide 1",
-      icon: "diamond-outline",
-      avatar: "https://pbs.twimg.com/media/EWSzU-WVcAE2HDW.jpg",
-      image: "https://media.slidesgo.com/storage/10149218/conversions/0-music-lesson-thumb.jpg",
-      description: "Texto de description"
-    },
-    {
-      title: "Titulo de slide 2",
-      icon: "diamond-outline",
-      avatar: "https://pbs.twimg.com/media/EWSzU-WVcAE2HDW.jpg",
-      image: "https://estaticos.elcolombiano.com/binrepository/1200x870/0c121/1200d627/none/11101/AHRM/nostalgia-musica-1_42537840_20230605193530.jpg",
-      description: "Texto de description"
-    },
-    {
-      title: "Titulo de slide 3",
-      icon: "diamond-outline",
-      avatar: "https://pbs.twimg.com/media/EWSzU-WVcAE2HDW.jpg",
-      image: "https://img.freepik.com/fotos-premium/musica-mente-arte-abstracto-musical-creado-tecnologia-generativa-ia_545448-15311.jpg",
-      description: "Texto de description"
-    },
-    {
-      title: "Titulo de slide 4 ",
-      icon: "diamond-outline",
-      avatar: "https://pbs.twimg.com/media/EWSzU-WVcAE2HDW.jpg",
-      image: "https://www.elhumanista.net/wp-content/uploads/2021/09/musica.jpg",
-      description: "Texto de description"
-    }
-  ]
+  
 
-  constructor() {}
+  constructor(private router: Router) {}
+  intro(){
+    this.router.navigateByUrl("/intro")
+  }
 
 }
