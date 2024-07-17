95e749309109893a36e7f3b5ffb892254044d0eb
Jhon Maldonado <jhonmaldonado01@gmail.com>
2024-07-17 16:13:56 -0500

    Tema 9: Se agregan validaciones en el formulario en campo email, password y button

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,14 +8,24 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
+  validation_messages = {
+    email:[
+      {type: "required", message: "El email es obligatorio"},
+      {type: "pattern", message: "Email invalido"}
+    ],
+    passkey:[
+      {type: "required", message:"Password invalido"}
+    ]
+  }
+
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       email: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          Validators.email
-          //Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.+$")
+          //Validators.email,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-Z0-9.]+$")
         ])
       ),
       passkey: new FormControl(
