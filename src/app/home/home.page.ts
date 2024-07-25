ae3a27a6bf8b4b4c5e0655ef3bfc1f1afaea37b3
Jhon Maldonado <jhonmaldonado01@gmail.com>
2024-07-25 13:04:15 -0500

    Tema 17 Se configura para extraer informacion de un servidor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MusicService } from '../services/music.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  artists = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
-  ]
 
-  slideOps = {
-    initialSlide: 2,
-    slidesPerView: 4,
-    canteredSlides: true,
-    speed: 400
-  }
+export class HomePage implements OnInit{
+  artistsJson: any;
+  artists: any;
 
-  constructor(private router: Router) {}
+  //slideOps = {
+  //  initialSlide: 2,
+  //  slidesPerView: 4,
+  //  canteredSlides: true,
+  //  speed: 400
+  //}
+
+  constructor(
+    private router: Router,
+    private musicService: MusicService) {}
+
   intro(){
     this.router.navigateByUrl("/intro")
+  }
+
+  ngOnInit() {
+    this.artistsJson = this.musicService.getArtistsJson().artists;
+    console.log("Json", this.artistsJson)
+
+    // Manera con HttpClient de extraer la informacion
+    //this.musicService.getArtists().subscribe((data:any) =>{
+    //  this.artists = data
+    //  console.log("Servidor", this.artists)
+    //})
+    
+    // Manera con Fetch de extraer la informacion
+    this.musicService.getArtists().then(data =>{
+      this.artists = data;
+      console.log(data)
+    })
   }
 
 }
---
+++ src/app/services/music.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as dataArtists from "./artists.json";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicService {
+  urlServer = "https://music.fly.dev"
+  httpHeaders = { headers: new HttpHeaders({"Content-Type": "application/json"})}
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getArtistsJson(){
+    return dataArtists;
+  }
+  
+  getArtists(){
+    //Maneja con HttpClient de extraer la informacion
+    //return this.http.get(`${this.urlServer}/artists`, this.httpHeaders)
+
+    // Metodo con Fetch de extraer la informacion
+    return fetch(`${this.urlServer}/artists`).then(
+      response => response.json()
+    )
+  }
+}
